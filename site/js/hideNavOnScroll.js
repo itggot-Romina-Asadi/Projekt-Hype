fb40d2e68e8dd3f1116c45d9214212a70b9d02b5
itggot-linus-styren <linus.styren@itggot.se>
2016-12-09 13:19:21 +0100

    added function to hide on scrolldown sortof, because we dont have any animation yet

--- site/js/hideNavOnScroll.js
+++ site/js/hideNavOnScroll.js
@@ -1,5 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
 	var nav = document.getElementsByTagName("nav")[0];
+	var target = $("article");
 
-	/*nav.style = "display: none";*/
+	$(document).on('scroll', function() {
+    	if($(this).scrollTop()>=target.position().top) {
+    		console.log("hide me!");
+        	nav.style = "display: none";
+        } else {
+        	nav.style = "display: flex";
+        }
+    });
 });
