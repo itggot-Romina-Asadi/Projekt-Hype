860338a01143469473cd854a815d71311d23f6d8
itggot-linus-styren <linus.styren@itggot.se>
2016-12-09 14:46:56 +0100

    dropdown button actually sortof works now!

--- site/js/buttonlistener.js
+++ site/js/buttonlistener.js
@@ -3,14 +3,28 @@ document.addEventListener('DOMContentLoaded', function() {
 	var dropdown = document.getElementById("dropdown");
 	var nav = document.getElementsByTagName("nav")[0];
 
-	newsbtn.addEventListener("mouseenter", function( event ) {
-		console.log("You are on the button!");
-		nav.style.overflowY = "visible";
-		dropdown.className = "dropdownhover";		
+	var isVis = false;
+
+	document.addEventListener("click", function( event ) {
+		if (isVis) {
+			isVis = false;
+		} else if (dropdown.className == "dropdownhover") {
+			dropdown.className = "";
+			nav.style.overflowY = "hidden";		
+		}
+		console.log("clicked somehwere in document");
 	});
 
+	newsbtn.addEventListener("click", function( event ) {
+		console.log("You clicked on the button!");
+		nav.style.overflowY = "visible";
+		dropdown.className = "dropdownhover";
+		isVis = true;	
+	});
+/*
 	dropdown.addEventListener("mouseleave", function( event ) {
 		dropdown.className = "";
 		nav.style.overflowY = "hidden";
 	});
+*/
 });
