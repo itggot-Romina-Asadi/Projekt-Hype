145af4e1168e11fd7bb4cdacd18a8378a2464af7
itggot-linus-styren <linus.styren@itggot.se>
2016-12-09 14:35:23 +0100

    fixed dropdown

--- site/js/buttonlistener.js
+++ site/js/buttonlistener.js
@@ -1,13 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
 	var btn = document.getElementById("newsbtn");
 	var dropdown = document.getElementById("dropdown");
+	var nav = document.getElementsByTagName("nav")[0];
 
 	newsbtn.addEventListener("mouseenter", function( event ) {
 		console.log("You are on the button!");
-		dropdown.className = "dropdownhover";
+		nav.style.overflowY = "visible";
+		dropdown.className = "dropdownhover";		
 	});
 
 	dropdown.addEventListener("mouseleave", function( event ) {
 		dropdown.className = "";
+		nav.style.overflowY = "hidden";
 	});
 });
